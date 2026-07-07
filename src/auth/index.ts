ed7cdb4a844266908a3978a47b6e7a66bc58eb4e
benmumma <ben@mumma.co>
2026-07-07 11:05:45 -0500

    feat(auth): public entry point

---
+++ src/auth/index.ts
@@ -0,0 +1,5 @@
+export { AuthClient } from './AuthClient';
+export { readSessionMarker, MARKER_COOKIE } from './marker';
+export { consumeBridgeHash } from './bridge';
+export { resolveAuthBaseUrl, buildSignInUrl, buildLogoutUrl, buildBridgeUrl } from './urls';
+export * from './types';
